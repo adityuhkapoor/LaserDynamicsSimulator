c30fa30e661aa9a5bd8e37a85955cc0a7e545135
Aditya K <41550011+adityuhkapoor@users.noreply.github.com>
2024-03-31 14:36:46 -0400

    Create simulatedGains.m

---
+++ simulatedGains.m
@@ -0,0 +1,16 @@
+function simulatedGains = SimulatedGainArray(endTime)
+    % Array of PumpPower_W_ values
+    pumpPowers = [7.81, 3699.22, 4714.84, 6375.00, 8289.06, 9285.16, 10222.66, 11218.75, 12214.84, 13113.28, 14109.38, 15105.47];
+    
+    % Array to store the simulated Single_PassGain values
+    simulatedGains = zeros(length(pumpPowers), 1);
+    
+    % Loop over each PumpPower_W_ value
+    for i = 1:length(pumpPowers)
+        % Call your simulation function
+        simulatedGains(i) = SSGainvsEndTimeandIp(pumpPowers(i), endTime);
+    end
+    
+    % Return the array of simulated gains
+    % No need for a separate return statement as the variable is returned automatically
+end
