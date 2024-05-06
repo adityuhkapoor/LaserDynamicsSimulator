0ce0acb5c57ba6845f26cbcc87bf37f87e6617a8
Aditya K <41550011+adityuhkapoor@users.noreply.github.com>
2024-05-06 16:27:07 -0400

    Update efficientMeritValueCalcArray.m

--- efficientMeritValueCalcArray.m
+++ efficientMeritValueCalcArray.m
@@ -1,4 +1,4 @@
-function efficientMeritValueCalcArray = MeritValCalc(x)
+function efficientMeritValueCalcArray = MeritValCalcArray(x)
     % Assuming x is an array with three elements [a, b, c]
     variedVarValue = x(1); % Use the first element for the original purpose
     pumpPowers = [7.81, 3699.22, 4714.84, 6375.00, 8289.06, 9285.16, 10222.66, 11218.75, 12214.84, 13113.28, 14109.38, 15105.47];
